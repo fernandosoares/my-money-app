059fe7558ac638aa4138711cef60476386f7e948
Fernando Soares <fernandosoares.dev@gmail.com>
2018-08-29 15:01:24 -0300

    Tabs

--- frontend/src/dashboard/dashboardReducer.js
+++ frontend/src/dashboard/dashboardReducer.js
@@ -3,7 +3,6 @@ const INITIAL_STATE = { summary: { credit: 0, debt: 0 } }
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'BILLING_SUMMARY_FETCH':
-        console.log('teste')
             return { ...state, summary: action.payload.data }
         default:
             return state
